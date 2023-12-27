8bcf5fd878c5a4f43576f6e22481fd2546b262c2
hesap <yildaiki@gmail.com>
2023-12-27 21:39:12 +0300

    buton için js de gerekli değişiklik yapıldı

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 let button = document.querySelector("a");
-button.forEach(btn => {
-    btn.addEventListener("click", function(s){
-        let z = s.clientX - s.target.offsetLeft;
-        let y = s.clientY - s.target.offsetTop;
+// let span1 = document.createElement("span");
+let body = document.getElementsByTagName("body")
+
+button.addEventListener('click', myFunction)
+
+function myFunction() {
+    var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16)
+    document.body.style.backgroundColor = randomColor
+    
+}
 
 
-        let span1 = document.createElement("span");
-        span1.style.left = z + "px";
-        span1.style.top = y + "px";
-        this.appendChild(span1);
 
-        setTimeout(() => {
-            span1.remove()
-        }, 800);
 
-    })
-});
+// button.forEach(btn => {
+//     btn.addEventListener("click", function(s){
+//         let z = s.clientX - s.target.offsetLeft;
+//         let y = s.clientY - s.target.offsetTop;
+
+
+//         let span1 = document.createElement("span");
+//         span1.style.left = z + "px";
+//         span1.style.top = y + "px";
+//         this.appendChild(span1);
+
+//         setTimeout(() => {
+//             span1.remove()
+//         }, 800);
+
+//     })
+// });
